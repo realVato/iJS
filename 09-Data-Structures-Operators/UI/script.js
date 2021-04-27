2963386a1b0310b32d5184b9fc8b699780485474
Tomas Talalas <tomcek99@gmail.com>
2021-04-27 09:14:07 +0300

    Update script.js

--- 09-Data-Structures-Operators/UI/script.js
+++ 09-Data-Structures-Operators/UI/script.js
@@ -49,6 +49,34 @@ const flights =
 // team1 < team2 && console.log(`team 1 is the shit`);
 // --------------------------------
 
+// #Challenge 2
+// --------------------------------
+//1.
+// const entries = Object.entries(game.scored);
+
+// for (let [i, el] of entries) {
+//   i = parseInt(i);
+//   console.log(`Goal ${i + 1}: ${el}`);
+// }
+
+// //2.
+// const values = Object.values(game.odds);
+// let avg = 0;
+
+// for (const odd of values) {
+//   avg += odd;
+// }
+
+// avg /= values.length;
+// console.log(avg);
+
+// //3.
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory for ${game[team]}`;
+//   console.log(`Odd of ${teamStr}: ${odd}`);
+// }
+// --------------------------------
+
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -90,27 +118,45 @@ const game = {
   },
 };
 
-//1.
-const entries = Object.entries(game.scored);
+// Challenge 3
+// --------------------------------
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
 
-for (let [i, el] of entries) {
-  i = parseInt(i);
-  console.log(`Goal ${i + 1}: ${el}`);
-}
+// GOOD LUCK 😀
+// */
 
-//2.
-const values = Object.values(game.odds);
-let avg = 0;
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
 
-for (const odd of values) {
-  avg += odd;
-}
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
 
-avg /= values.length;
-console.log(avg);
+// 2.
+gameEvents.delete(64);
 
-//3.
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `victory for ${game[team]}`;
-  console.log(`Odd of ${teamStr}: ${odd}`);
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes.`
+);
+
+// 4.
+for (const [key, value] of gameEvents) {
+  const half = key <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${key}: ${value}`);
 }
